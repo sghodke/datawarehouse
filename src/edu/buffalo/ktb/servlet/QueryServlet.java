8cf2999a4cfbf822abbb74a8fd90c9aaeef07627
amitnihalani <ami.nihalani@gmail.com>
2015-10-10 02:17:10 -0400

    Added UI Changes + Servlet

--- src/edu/buffalo/ktb/servlet/QueryServlet.java
+++ src/edu/buffalo/ktb/servlet/QueryServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.buffalo.ktb.bean.Patient;
 import edu.buffalo.ktb.service.QueryService;
 
 /**
@@ -29,77 +30,70 @@ public class QueryServlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-			IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String queryNo = request.getParameter("queryNo");
+		List<Patient> patientList = queryService.getPatients();
+		System.out.println(patientList.size() + "\n");
 
-		switch (queryNo) {
-		case "1":
-			String description = request.getParameter("description");
-			String name = request.getParameter("name");
-			String type = request.getParameter("type");
-			Map<String, Integer> resultQueryOne = queryService.getResultQueryOne(description, name, type);
+		String deseaseDescription, deseaseName, deseaseType, clusterId, measureUnitId, goId;
+
+		if (request.getParameter("button_query1") != null) {
+			deseaseDescription = request.getParameter("description_query1");
+			deseaseName = request.getParameter("name_query1");
+			deseaseType = request.getParameter("type_query1");
+			Map<String, Integer> resultQueryOne = queryService.getResultQueryOne(deseaseDescription, deseaseName, deseaseType);
 			Set<String> keys = resultQueryOne.keySet();
-			for (String key : keys) {
+			for(String key: keys) {
 				System.out.println(key + ": " + resultQueryOne.get(key));
 			}
-			break;
-		case "2":
-			String description1 = request.getParameter("description2");
-			List<String> resultQueryTwo = queryService.getResultQueryTwo(description1);
-			System.out.println("Total number of results" + resultQueryTwo.size());
+		} else if (request.getParameter("button_query2") != null) {
+			deseaseDescription=request.getParameter("description_query2");
+			List<String> resultQueryTwo=queryService.getResultQueryTwo(deseaseDescription);
+			System.out.println("Total number of results: "+resultQueryTwo.size());
 			System.out.println("Drug Types:-->\n");
-			for (String str : resultQueryTwo) {
+			for(String str:resultQueryTwo){
 				System.out.println(str);
 			}
-			break;
-
-		case "4":
-			String goIdFour = request.getParameter("goIdFour");
-			String dsName = request.getParameter("dsName");
-			double tstat = queryService.getResultQueryFour(goIdFour, dsName);
-			System.out.println("T-stat: " + tstat);
-			break;
-
-		case "5":
-			String goId = request.getParameter("goId");
-			String dsNameOne = request.getParameter("dsNameOne");
-			String dsNameTwo = request.getParameter("dsNameTwo");
-			String dsNameThree = request.getParameter("dsNameThree");
-			String dsNameFour = request.getParameter("dsNameFour");
-			double fstat = queryService.getResultQueryFive(goId, dsNameOne, dsNameTwo, dsNameThree, dsNameFour);
-			System.out.println("F-stat:" + fstat);
-			break;
-
-		case "6":
-			String goIdSix = request.getParameter("goIdSix");
-			double[] correlation = queryService.getResultQuerySix(goIdSix);
-			if (correlation != null) {
-				System.out.println("Correlation between two patients with ALL is-->" + correlation[0]);
-				System.out.println("Correlation between two patients with ALL and AML is-->" + correlation[1]);
+		} else if (request.getParameter("button_query3") != null) {
+			deseaseName = request.getParameter("name_query3");
+			clusterId = request.getParameter("cluster_id_query3");
+			measureUnitId = request.getParameter("measure_unid_id_query3");
+			List<Integer> resultQueryThree=queryService.getResultQueryThree(clusterId, measureUnitId, deseaseName);
+			System.out.println("Total number of results: "+resultQueryThree.size());
+			System.out.println("mRNA values:-->\n");
+			for(int i:resultQueryThree){
+				System.out.println(i);
 			}
-			break;
-
-		case "7":
-			queryService.getResultQueryPart3A();
-			break;
-
-		default:
-			break;
+		} else if (request.getParameter("button_query4") != null){
+			deseaseName = request.getParameter("name_query4");
+			goId = request.getParameter("go_id_query4");
+			Double resultQueryFour = queryService.getResultQueryFour(goId, deseaseName);
+			System.out.println("T Stat " + resultQueryFour);
+		} else if(request.getParameter("button_query5") != null){
+			String deseaseNameOne = request.getParameter("name1_query5");
+			String deseaseNameTwo = request.getParameter("name2_query5");
+			String deseaseNameThree = request.getParameter("name3_query5");
+			String deseaseNameFour = request.getParameter("name3_query5");
+			goId = request.getParameter("go_id_query5");
+			Double resultQueryFive = queryService.getResultQueryFive(goId, deseaseNameOne, deseaseNameTwo, deseaseNameThree, deseaseNameFour);
+			System.out.println("F Stat " + resultQueryFive);
+		} else if(request.getParameter("button_query6") != null) { 
+			goId = request.getParameter("go_id_query6");
+			double[] correlation=queryService.getResultQuerySix(goId);
+			if(correlation!=null){
+				System.out.println("Correlation between two patients with ALL is-->"+correlation[0]);
+				System.out.println("Correlation between two patients with ALL and AML is-->"+correlation[1]);
+			}
 		}
-
 	}
 
 }
